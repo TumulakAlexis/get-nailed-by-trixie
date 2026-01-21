35e2eb4d6ddc8484cd3fee5f6559490eb0f1cf5a
TumulakAlexis <alexistumulak26@gmail.com>
2026-01-21 15:24:16 +0800

    minor fixes

--- src/landing-page/section1.jsx
+++ src/landing-page/section1.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import './section1.css';
+import Landing from '../assets/landing/landing.png'
+import { HashLink } from 'react-router-hash-link/dist/react-router-hash-link.cjs.production';
 
 export default function Section1() {
     return (
         <section className="hero-container">
             <div className="hero-image-side">
                 <img
-                    src="https://images.unsplash.com/photo-1632345031435-8727f6897d53?q=80&w=1000&auto=format&fit=crop"
+                    src= {Landing}
                     alt="Luxury Nail Art"
                     className="hero-img"
                 />
@@ -28,7 +29,9 @@ export default function Section1() {
                     <p className="hero-quote">
                         “Where Luxury Meets <br /> Perfectly Crafted Nails.”
                     </p>
-                    <button className="btn-services">Services</button>
+                    <HashLink smooth to="/#services">
+                            <button className="btn-services">Services</button>
+                        </HashLink>
                 </div>
             </div>
         </section>
