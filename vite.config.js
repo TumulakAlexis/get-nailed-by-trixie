6f70e99b6b4122f4855f8edce8f031391e1533da
TumulakAlexis <alexistumulak26@gmail.com>
2026-08-21 11:34:26 +0800

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteSitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteSitemap({
+      hostname: 'https://www.getnailedbytrixie.online',
+    }),
+  ],
 })
